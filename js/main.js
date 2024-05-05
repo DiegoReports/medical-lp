f72a60cfdbca921f413ed9e3b6e78d99c24514ca
Diego Henrique <diego.reports@gmail.com>
2024-05-05 16:53:18 -0300

    Update: Add Testimonials Section

--- js/main.js
+++ js/main.js
@@ -28,3 +28,14 @@ closeBtn.addEventListener('click', () => {
     closeBtnIcn.classList.toggle(arrowRightClass)
   }
 })
+
+/* SWIPER */
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+})
